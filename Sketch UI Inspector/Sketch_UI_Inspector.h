17f64cced6ad15fd5b4574b96c441712b14fc15a
Sergey Dmitriev <el.pajaro.sabio@gmail.com>
2018-09-28 22:09:54 +0300

    Initial Commit

---
+++ Sketch UI Inspector/Sketch_UI_Inspector.h
@@ -0,0 +1,19 @@
+//
+//  Sketch_UI_Inspector.h
+//  Sketch UI Inspector
+//
+//  Created by Sergey Dmitriev on 28/09/2018.
+//  Copyright © 2018 Sergey Dmitriev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Sketch_UI_Inspector.
+FOUNDATION_EXPORT double Sketch_UI_InspectorVersionNumber;
+
+//! Project version string for Sketch_UI_Inspector.
+FOUNDATION_EXPORT const unsigned char Sketch_UI_InspectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sketch_UI_Inspector/PublicHeader.h>
+
+
